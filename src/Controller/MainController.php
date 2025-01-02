6128e1747d8098ea27dcb728f3a71949602a29d3
Yen <y.rod.vas@gmail.com>
2025-01-02 19:46:39 +0100

    Añadir lógica para mostrar productos desde la base de datos en la página Tienda

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use App\Repository\ProductoRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,11 +15,15 @@ class MainController extends AbstractController
         return $this->render('main/index.html.twig');
     }
 
-    #[Route('/tienda', name: 'tienda')]
-    public function tienda(): Response
-    {
-        return $this->render('main/tienda.html.twig');
-    }
+    #[Route('/tienda', name: 'main_tienda')]
+    public function tienda(ProductoRepository $productoRepository): Response
+{
+    $productos = $productoRepository->findAll();
+
+    return $this->render('main/tienda.html.twig', [
+        'productos' => $productos,
+    ]);
+}
 
     #[Route('/blog', name: 'blog')]
     public function blog(): Response
