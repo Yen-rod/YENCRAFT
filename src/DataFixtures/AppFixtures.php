4d615a2da55df04071148e99be1dbead093957a4
Yen <y.rod.vas@gmail.com>
2025-01-02 19:40:31 +0100

    Generación de Fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Producto;
+use App\Entity\Categoria;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // Crear categorías
+        $categoria1 = new Categoria();
+        $categoria1->setNombre('Lámparas');
+        $manager->persist($categoria1);
+
+        $categoria2 = new Categoria();
+        $categoria2->setNombre('Cuadros');
+        $manager->persist($categoria2);
+
+        // Crear productos
+        $producto1 = new Producto();
+        $producto1->setNombre('Lámpara de Madera');
+        $producto1->setPrecio(45.99);
+        $producto1->setCategoria($categoria1); // Relación con la categoría
+        $manager->persist($producto1);
+
+        $producto2 = new Producto();
+        $producto2->setNombre('Cuadro de Roble');
+        $producto2->setPrecio(75.50);
+        $producto2->setCategoria($categoria2); // Relación con la categoría
+        $manager->persist($producto2);
+
+        $manager->flush();
+    }
+}
+
