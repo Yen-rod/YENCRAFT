cf9d850d4d02692aecf4df3d0d0b0c4b89df0438
Yen <y.rod.vas@gmail.com>
2025-01-21 15:56:20 +0100

    Configuración y corrección de TailwindCSS para el proyecto YenCraft

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './templates/**/*{html,js}', // Asegúrate de que incluye las plantillas Twig
+  ],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+};
